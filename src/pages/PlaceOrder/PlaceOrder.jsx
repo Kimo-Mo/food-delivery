5a2242c25c1b49e8c491357ce8614b125bd701f5
Kareem Mohamed <kimomo844@gmail.com>
2024-08-31 08:18:21 +0300

    Fix Delivery Fee

--- src/pages/PlaceOrder/PlaceOrder.jsx
+++ src/pages/PlaceOrder/PlaceOrder.jsx
@@ -8,20 +8,25 @@ const PlaceOrder = () => {
       <div className="place-order-left">
         <h2 className="title">Delivery Information</h2>
         <div className="multi-fields">
-          <input type="text" name="firstName" placeholder="First Name" required/>
-          <input type="text" name="lastName" placeholder="Last Name" required/>
+          <input
+            type="text"
+            name="firstName"
+            placeholder="First Name"
+            required
+          />
+          <input type="text" name="lastName" placeholder="Last Name" required />
         </div>
-        <input type="email" name="email" placeholder="Email Address" required/>
-        <input type="text" name="street" placeholder="Street"required />
+        <input type="email" name="email" placeholder="Email Address" required />
+        <input type="text" name="street" placeholder="Street" required />
         <div className="multi-fields">
-          <input type="text" name="city" placeholder="City" required/>
-          <input type="text" name="state" placeholder="State" required/>
+          <input type="text" name="city" placeholder="City" required />
+          <input type="text" name="state" placeholder="State" required />
         </div>
         <div className="multi-fields">
-          <input type="text" name="zipCode" placeholder="Zip Code" required/>
-          <input type="text" name="country" placeholder="Country" required/>
+          <input type="text" name="zipCode" placeholder="Zip Code" required />
+          <input type="text" name="country" placeholder="Country" required />
         </div>
-        <input type="text" name="phone" placeholder="Phone Number" required/>
+        <input type="text" name="phone" placeholder="Phone Number" required />
       </div>
       <div className="place-order-right">
         <div className="cart-total">
@@ -34,7 +39,7 @@ const PlaceOrder = () => {
             <hr />
             <div className="cart-total-details">
               <p>Delivery Fee</p>
-              <p>${parseInt(getTotalCartAmount() * 0.05)}</p>
+              <p>${(getTotalCartAmount() * 0.05).toFixed(2)}</p>
             </div>
             <hr />
             <div className="cart-total-details">
