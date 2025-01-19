77d5f6b1d208e638be9e9e56851268d7eaad249d
kareem mohamed <kimomo844@gmail.com>
2025-01-19 14:53:31 +0200

    scroll to top

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
 import Home from "./pages/Home/Home";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Footer from "./components/Footer/Footer";
 import Loading from "./components/Loading/Loading";
 
@@ -15,6 +15,10 @@ const LoginPopup = React.lazy(() =>
 const App = () => {
   const [showLogin, setShowLogin] = useState(false);
   const [user, setUser] = useState(null);
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
   return (
     <>
       {showLogin && (
@@ -22,30 +26,29 @@ const App = () => {
           <LoginPopup setShowLogin={setShowLogin} setUser={setUser} />
         </React.Suspense>
       )}
-      
-        <NavBar setShowLogin={setShowLogin} user={user} setUser={setUser} />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Home" element={<Home />} />
-          <Route
-            path="/Cart"
-            element={
-              <React.Suspense fallback={<Loading />}>
-                <Cart />
-              </React.Suspense>
-            }
-          />
-          <Route
-            path="/Order"
-            element={
-              <React.Suspense fallback={<Loading />}>
-                <PlaceOrder />
-              </React.Suspense>
-            }
-          />
-        </Routes>
-        <Footer />
-      
+
+      <NavBar setShowLogin={setShowLogin} user={user} setUser={setUser} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Home" element={<Home />} />
+        <Route
+          path="/Cart"
+          element={
+            <React.Suspense fallback={<Loading />}>
+              <Cart />
+            </React.Suspense>
+          }
+        />
+        <Route
+          path="/Order"
+          element={
+            <React.Suspense fallback={<Loading />}>
+              <PlaceOrder />
+            </React.Suspense>
+          }
+        />
+      </Routes>
+      <Footer />
     </>
   );
 };
